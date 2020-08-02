7f8bf23b3e3a05849a3a3660be5b095e9a9d2a2d
jungjh123456 <jungjh123456@naver.com>
2020-08-02 23:05:42 +0900

    javascript 중

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-let menu = document.querySelector('.nav__menuItem');
+// let liList = document.querySelectorAll('.nav__menuItem a');
+// let liList1 = document.querySelectorAll('.nav__subMenu');
+
+
+// let newArr = Array.from(liList);
+
+
+// newArr[0].addEventListener('mouseover', function() {
+//   toggleElements();
+// })
+
+
+
+
+// function toggleElements(){
+//   [].forEach.call(liList1, function(toggle){
+//     toggle.classList.toggle('isAct');
+//   })
+// }
