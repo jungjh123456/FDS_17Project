9a96170cc44ee6e4feb0bb27a23e66f9156d8348
jungjh123456 <jungjh123456@naver.com>
2020-08-03 10:12:21 +0900

    header

--- js/main.js
+++ js/main.js
@@ -17,3 +17,35 @@
 //     toggle.classList.toggle('isAct');
 //   })
 // }
+
+// let btn = document.getElementById('btn');
+// let liList = document.querySelector('.nav__subMenu');
+
+// btn.addEventListener('mouseover', function() {
+//   toggleElements();
+// })
+
+// function toggleElements(){
+//      [].forEach.call(liList, function(toggle){
+//        toggle.classList.toggle('isAct');
+//      })
+//    }
+
+(function(window,document){
+  'use strict'
+let searchBtn = document.querySelector('.form__btnSearch');
+
+
+
+searchBtn.addEventListener('mouseover', function(){
+  let searchInput = document.getElementById('searchInput');
+  searchInput.getAttribute('class');
+  searchInput.setAttribute('class','isSearch');
+});
+
+// window.addEventListener('mouseover', function() {
+//   let searchInput = document.getElementById('searchInput');
+//   searchInput.getAttribute('class');
+//   searchInput.classList.remove('isSearch');
+// })
+})(window,document)
