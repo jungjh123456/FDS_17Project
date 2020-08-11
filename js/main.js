c13ef7c1eeb16d70f07110582066b3a594e5de2f
jungjh123456 <jungjh123456@naver.com>
2020-08-11 10:35:27 +0900

    반응형 스타벅스

--- js/main.js
+++ js/main.js
@@ -16,32 +16,51 @@
 (function( document){
   'use strict'
 
-  const $nav = document.getElementById('nav__menu');
-  console.log($nav);
-  //isAct 추가
-  $nav.onmouseover =(e) => {
-    if(!e.target.matches('.nav__menu > li > a')) {
-      return;
-    }
-    console.log(e.target);
-    const subM =e.target.nextElementSibling; /*<- ul : nav__subMenu  */
-    e.target.nextElementSibling.classList.add('isAct');
-    // test.classList.add('isAct');
-    
-    // isAct 제거a
-    $nav.onmouseout =(e) => {
-      if(!e.target.matches('.nav__menu > li > a ')) {
-        return;
-      }
-      e.target.nextElementSibling.classList.remove('isAct');
-    }
-    subM.onmouseover = e =>{
-      subM.classList.add('isAct')
-      subM.onmouseout = e => {
-        subM.classList.remove('isAct')
-      }
-    }
+  const $nav = document.querySelectorAll('.buttonBurger');
+  const $list = document.querySelector('.flex-group');
+  let listtoggle = Array.from($nav);
+  console.log(listtoggle);
+
+  listtoggle[1].onclick = function(){
+    const $list = document.querySelector('.flex-group');
+    console.log(listtoggle[1]);
+    console.log($list);
+    $list.classList.toggle('isAct');
   }
+
+  listtoggle[0].onclick = function(){
+    const $list = document.querySelector('.flex-group');
+    $list.classList.remove('isAct');
+  }
+
+
+
+
+
+  //isAct 추가
+  // $nav.onmouseover =(e) => {
+  //   if(!e.target.matches('.nav__menu > li > a')) {
+  //     return;
+  //   }
+  //   console.log(e.target);
+  //   const subM =e.target.nextElementSibling; /*<- ul : nav__subMenu  */
+  //   e.target.nextElementSibling.classList.add('isAct');
+  //   // test.classList.add('isAct');
+    
+  //   // isAct 제거a
+  //   $nav.onmouseout =(e) => {
+  //     if(!e.target.matches('.nav__menu > li > a ')) {
+  //       return;
+  //     }
+  //     e.target.nextElementSibling.classList.remove('isAct');
+  //   }
+  //   subM.onmouseover = e =>{
+  //     subM.classList.add('isAct')
+  //     subM.onmouseout = e => {
+  //       subM.classList.remove('isAct')
+  //     }
+  //   }
+  // }
 
 
 })( document)
